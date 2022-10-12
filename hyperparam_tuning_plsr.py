855d7a0ffbe2562db13a04da14d783edf0ff5b2e
Gabriella Chronis <gigichronis@gmail.com>
2022-10-11 19:52:42 -0500

    use ray tune to streamline and doublecheck hyperparam search

--- hyperparam_tuning_plsr.py
+++ hyperparam_tuning_plsr.py
@@ -1,46 +1,64 @@
 """
-script to do hyperparameter tuning for ffnn on mcrae data
+script to do hyperparameter tuning for PLSR
 """
 import subprocess, os
+from ray import tune
+import classifier_main
+from ray.tune.schedulers import ASHAScheduler
 
 
 if __name__ == '__main__':
 
 	models = ['plsr']
-	#datasets = ['mc_rae_real', 'buchanan']
-	datasets = ['binder']
-	embeddings = ['5k', '1k', 'glove']
+	#datasets = ['binder', 'mc_rae_real', 'buchanan']
+	datasets = ['binder', 'mc_rae_real'] # dont actually tune buchanan it just takes too long; use mcrae params
+	embeddings = ['bert', 'glove']
+	clusters = [1]
 	plsr_n_components = ['30', '50', '100', '300']
-	#plsr_n_components = ['300']
 	plsr_max_iters = ['500']
-	  
 
-	for model in models:
-		for dataset in datasets:
-			for embedding in embeddings:
-				for plsr_n_component in plsr_n_components:
-					for plsr_max_iter in plsr_max_iters:
-						command = [
-							"python3",
-							"classifier_main.py",
-							"--print_dataset",
-							"--model=plsr",
-							"--train_data=" + str(dataset),
-							'--plsr_n_components=' + str(plsr_n_component) ,
-							'--plsr_max_iter=' + str(plsr_max_iter)
-							]
 
-						if embedding == '1k':
-							command.append("--layer=8 --clusters=1")
-						elif embedding =='5k':
-							command.append("--layer=8 --clusters=5")
-						elif embedding == 'glove':
-							command.append("--embedding_type=glove")
+	config = {
+			"seed": 42,
+			"layer": 8,
+			"clusters": tune.grid_search(clusters),
+			"embedding_type": tune.grid_search(embeddings),
+			"model": tune.choice(models),
+			"train_data": tune.choice(datasets),
 
-						save_path = 'trained_models/model.plsr.' + dataset + '.' + embedding + '.' + plsr_n_component + 'components.' + plsr_max_iter + 'max_iters'
-						command.append('--save_path=' + save_path)
+			"plsr_n_components": plsr_n_components
+			"plsr_max_iter": plsr_max_iters,	
 
-						print("running command:")
-						print(command)
+		    "TUNE_ORIG_WORKING_DIR": os.getcwd(),
 
-						os.system(' '.join(command))
+		    "kfold": True,
+
+		    # BS stuff??
+		    "print_dataset": False,
+		    "save_path": None,
+		    "do_dumb_thing": False,
+		    "dev_equals_train": False,
+		    "tuning": True,
+		    "allbuthomonyms": False
+			}
+
+
+	run_name = plsr_tuning_kfold_10_11_2022
+	analysis = tune.run(
+		classifier_main.main,
+		config=config,
+		scheduler=ASHAScheduler(metric="MAP_at_k", mode="max"),
+		
+		# DEBUG
+		#num_samples=25,
+		num_samples = 1,
+    	#name="main_2022-02-11_15-08-47",
+    	name=run_name,
+    	#trial_name_creator = tune.function(lambda trial: trial.config['embedding_type'] + str(trial.config['clusters']) + '_' + trial.trial_id),
+    	#resume="AUTO"
+    	resume = False
+	)
+
+	# Obtain a trial dataframe from all run trials of this `tune.run` call.
+	dfs = analysis.trial_dataframes
+	dfs.to_csv(name+'.csv')
